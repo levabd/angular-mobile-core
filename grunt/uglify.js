cf38aaf91210665bbd5d972eb423c07f5d4cfe95
Lucas Nova <pogorelowMax@gmail.com>
2016-01-17 13:27:51 +0600

    build file

---
+++ grunt/uglify.js
@@ -0,0 +1,11 @@
+module.exports = {
+    default: {
+        options: {
+            beautify: false,
+            mangle: false,
+            compress: {}
+        },
+        src: ['src/**/*.js'],
+        dest: 'build/angular-mobile-core.js'
+    }
+};
